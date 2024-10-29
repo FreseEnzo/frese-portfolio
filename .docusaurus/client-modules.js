e866955f9a62a21c74b336042dcb9dc714fdbfff
Frese <enzo.frese@hardwario.com>
2024-10-29 10:45:22 -0300

    portfolio: Start docs

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/frese/personal_projects/frese-portfolioa/node_modules/infima/dist/css/default/default.css"),
+  require("/home/frese/personal_projects/frese-portfolioa/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/frese/personal_projects/frese-portfolioa/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/frese/personal_projects/frese-portfolioa/src/css/custom.css"),
+];
